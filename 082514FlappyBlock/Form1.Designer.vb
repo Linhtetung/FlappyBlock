1e840f6e216783418ca2fff8ec8afdf97585f053
linhtetung <lhtetaung68@gmail.com>
2019-12-29 22:52:41 +0400

    first commit

---
+++ 082514FlappyBlock/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.picFlappy = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picFlappy, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.Color.Green
+        Me.PictureBox6.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox6.Location = New System.Drawing.Point(536, -1)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(32, 71)
+        Me.PictureBox6.TabIndex = 6
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Green
+        Me.PictureBox5.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox5.Location = New System.Drawing.Point(536, 185)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(32, 259)
+        Me.PictureBox5.TabIndex = 5
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.Color.Green
+        Me.PictureBox4.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox4.Location = New System.Drawing.Point(375, 373)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(31, 71)
+        Me.PictureBox4.TabIndex = 4
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Green
+        Me.PictureBox3.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox3.Location = New System.Drawing.Point(375, -1)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(31, 260)
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Green
+        Me.PictureBox2.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox2.Location = New System.Drawing.Point(203, -1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(33, 123)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Green
+        Me.PictureBox1.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources.images21
+        Me.PictureBox1.Location = New System.Drawing.Point(203, 241)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(33, 203)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'picFlappy
+        '
+        Me.picFlappy.BackColor = System.Drawing.Color.Red
+        Me.picFlappy.Image = Global._082514FlappyBlock.My.Resources.Resources.index
+        Me.picFlappy.Location = New System.Drawing.Point(56, 197)
+        Me.picFlappy.Name = "picFlappy"
+        Me.picFlappy.Size = New System.Drawing.Size(20, 20)
+        Me.picFlappy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picFlappy.TabIndex = 0
+        Me.picFlappy.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Navy
+        Me.Label1.Location = New System.Drawing.Point(80, 125)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(295, 93)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Press Enter to Play" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " Press Spacebar to flap" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 60
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global._082514FlappyBlock.My.Resources.Resources._0029
+        Me.ClientSize = New System.Drawing.Size(580, 441)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.picFlappy)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "FlappyBlock By C.Striker"
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picFlappy, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picFlappy As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ 082514FlappyBlock/Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form1
+    Dim iScore As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'picFlappy default location is set
+        picFlappy.Location = New Point(Me.Size.Width / 6, Me.Size.Height / 2)
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+
+    End Sub
+
+    Sub flappyDrop()
+        picFlappy.Top += 10
+    End Sub
+
+    Sub checkBottom()
+        If picFlappy.Location.Y > Me.Height - picFlappy.Size.Height - 45 Then
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+            MsgBox("Game Over!")
+        End If
+    End Sub
+
+    Sub checkTop()
+        If picFlappy.Location.Y < 0 Then
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+            MsgBox("Game Over!")
+        End If
+    End Sub
+    Sub detectCollision()
+        If PictureBox1.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox1.Right Then
+            If PictureBox1.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox1.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+        If PictureBox2.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox2.Right Then
+            If PictureBox2.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox2.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+        If PictureBox3.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox3.Right Then
+            If PictureBox3.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox3.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+        If PictureBox4.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox4.Right Then
+            If PictureBox4.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox4.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+        If PictureBox5.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox5.Right Then
+            If PictureBox5.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox5.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+        If PictureBox6.Left < picFlappy.Left AndAlso picFlappy.Left < PictureBox6.Right Then
+            If PictureBox6.Top < picFlappy.Top AndAlso picFlappy.Top < PictureBox6.Bottom Then
+                Timer1.Enabled = False
+                Timer2.Enabled = False
+                MsgBox("Game Over!")
+            End If
+        End If
+
+    End Sub
+
+    Sub movBlock()
+        If PictureBox1.Location.X < 0 Then
+            PictureBox1.Location = New Point(Me.Width, PictureBox1.Location.Y)
+            iScore = iScore + 1
+
+        Else
+            PictureBox1.Left -= 5
+        End If
+        If PictureBox2.Location.X < 0 Then
+            PictureBox2.Location = New Point(Me.Width, PictureBox2.Location.Y)
+        Else
+            PictureBox2.Left -= 5
+        End If
+        If PictureBox3.Location.X < 0 Then
+            PictureBox3.Location = New Point(Me.Width, PictureBox3.Location.Y)
+            iScore = iScore + 1
+
+        Else
+            PictureBox3.Left -= 5
+        End If
+        If PictureBox4.Location.X < 0 Then
+            PictureBox4.Location = New Point(Me.Width, PictureBox4.Location.Y)
+        Else
+            PictureBox4.Left -= 5
+        End If
+        If PictureBox5.Location.X < 0 Then
+            PictureBox5.Location = New Point(Me.Width, PictureBox5.Location.Y)
+        Else
+            PictureBox5.Left -= 5
+        End If
+        If PictureBox6.Location.X < 0 Then
+            PictureBox6.Location = New Point(Me.Width, PictureBox6.Location.Y)
+            iScore = iScore + 1
+
+        Else
+            PictureBox6.Left -= 5
+        End If
+    End Sub
+
+    Sub showScores()
+        If Timer1.Enabled = False And Timer2.Enabled = False Then
+            MsgBox("Your score is " & iScore)
+            Me.Close()
+        End If
+    End Sub
+
+    Sub speed()
+        Select Case iScore
+            Case 6
+                Timer2.Interval = 35
+            Case 12
+                Timer2.Interval = 30
+            Case 18
+                Timer2.Interval = 25
+            Case 24
+                Timer2.Interval = 20
+            Case 30
+                Timer2.Interval = 15
+            Case 36
+                Timer2.Interval = 10
+            Case 42
+                Timer2.Interval = 5
+            Case 48
+                Timer2.Interval = 0
+        End Select
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If Timer1.Enabled = True And Timer2.Enabled = True Then
+            If e.KeyValue = Keys.Space Then
+                picFlappy.Top -= 50
+            End If
+        End If
+        If e.KeyValue = Keys.Enter Then
+            Timer1.Enabled = True
+            Timer1.Interval = 70
+            Timer2.Enabled = True
+            Timer2.Interval = 35
+            Label1.Visible = False
+        End If
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        flappyDrop()
+        checkBottom()
+        checkTop()
+        detectCollision()
+        showScores()
+        speed()
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        movBlock()
+    End Sub
+End Class
